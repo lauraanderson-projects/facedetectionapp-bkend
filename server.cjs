459a48b0b32568c4c68df2b53f4623735e0c9698
sphirebluela <laura.sphireblue@gmail.com>
2025-08-13 14:46:05 -0700

    port change

--- server.cjs
+++ server.cjs
@@ -55,6 +55,6 @@ app.post("/imageurl", (req, res) =>
   clarifaiimage.handleApiCall(req, res, fetch, PAT)
 );
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
