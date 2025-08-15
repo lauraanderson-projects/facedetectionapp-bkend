d3193e9e188e015f400a78f63998b6ded5f7198f
sphirebluela <laura.sphireblue@gmail.com>
2025-08-14 22:56:19 -0700

    postgres connection url

--- server.cjs
+++ server.cjs
@@ -19,12 +19,11 @@ const clarifaiimage = require("./controllers/clarifaiimage.js");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false },
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
   },
 });
 
